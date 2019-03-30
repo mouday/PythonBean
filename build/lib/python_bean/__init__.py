36740501d15551da80446ddbd4aa0a0078a3a63f
彭世瑜 <psy068@qimingpian.com>
2019-03-30 14:55:01 +0800

    delete

--- build/lib/python_bean/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# @Date    : 2019-03-30
-# @Author  : Peng Shiyu
-
-from __future__ import unicode_literals, print_function
-from .database import DataBase
-from .document_similar import DocumentSimilar
-
